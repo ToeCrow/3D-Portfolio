8c3c6ac0849fb1d1e5424d250e459de0663d324c
ToeCrow <thomas.kronvold@chasacademy.se>
2025-06-17 17:16:48 +0200

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+//src\App.tsx
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Stars } from '@react-three/drei';
 import Universe from './components/Universe';
 
 function App() {
   return (
-    <Canvas camera={{ position: [0, 0, 10], fov: 75 }}>
+    <Canvas 
+      camera={{ position: [0, 0, 10], fov: 75 }}
+      style={{ width: '100vw', height: '100vh' }}
+      gl={{ antialias: true }}>
       <ambientLight intensity={0.5} />
       <pointLight position={[0, 0, 0]} intensity={1.5} />
+      <color attach="background" args={['black']} />
       <Stars />
       <Universe />
       <OrbitControls />
